26ce999c269c9aa4939079a210caab7642a4a7ab
esma_akdemir <1esmakdemir@gmail.com>
2023-06-08 22:36:01 +0300

    zam_hakki metodu

--- MaviYaka.py
+++ MaviYaka.py
@@ -9,3 +9,27 @@ class MaviYaka(Calisan):
 
     def set_yipranma_payi(self, yipranma_payi):
         self.__yipranma_payi = yipranma_payi
+
+    def zam_hakki(self):# Zam hesaplama yöntemi
+        try:
+            if self.get_tecrube() >= 2 and self.get_tecrube() <= 4 and self.get_maas() < 15000:
+                # Eğer çalışanın tecrübesi 2 ile 4 yıl arasında ve maaşı 15000 TL'den düşükse
+                # Zam oranı, maaşın tecrübeye bölümünden kalanının yarısıyla yıpranma payının 10 katı arasındadır
+                zam_orani = (self.get_maas() % self.get_tecrube()) / 2 + (self.get_yipranma_payi() * 10)
+                # Zam miktarı, zam oranının maaşın yüzdesiyle çarpılarak hesaplanır
+                self.set_yeni_maas(self.get_maas() + (zam_orani * self.get_maas() / 100))
+            elif self.get_tecrube() > 4 and self.get_maas() < 25000:
+                # Eğer çalışanın tecrübesi 4 yıldan fazla ve maaşı 25000 TL'den düşükse
+                # Zam oranı, maaşın tecrübeye bölümünden kalanının üçte biriyle yıpranma payının 10 katı arasındadır
+                zam_orani = (self.get_maas() % self.get_tecrube()) / 3 + (self.get_yipranma_payi() * 10)
+                # Zam miktarı, zam oranının maaşın yüzdesiyle çarpılarak hesaplanır
+                self.set_yeni_maas(self.get_maas() + (zam_orani * self.get_maas() / 100))
+            else:
+                # Diğer durumlarda sadece yıpranma payının 10 katı zam yapılır
+                zam_orani = (self.get_yipranma_payi() * 10)
+                # Zam miktarı, zam oranının maaşın yüzdesiyle çarpılarak hesaplanır
+                self.set_yeni_maas(self.get_maas() + (zam_orani * self.get_maas() / 100))
+
+        # Hata durumunda gerekli işlemler yapılır
+        except ZeroDivisionError as e:
+            print("Hata: Sıfıra bölme hatası - Tecrübe değeri sıfır olamaz.")
