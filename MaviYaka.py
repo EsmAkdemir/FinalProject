67aa11bc9c318cb5962c4205237d09d706997f60
esma_akdemir <1esmakdemir@gmail.com>
2023-06-08 22:34:26 +0300

    initializer ve get/set metodu

---
+++ MaviYaka.py
@@ -0,0 +1,11 @@
+from Calisan import Calisan
+class MaviYaka(Calisan):
+    def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas, yeni_maas, yipranma_payi):
+        super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas, yeni_maas)
+        self.__yipranma_payi = yipranma_payi
+
+    def get_yipranma_payi(self):
+        return self.__yipranma_payi
+
+    def set_yipranma_payi(self, yipranma_payi):
+        self.__yipranma_payi = yipranma_payi
