d894e8f69c702db19757c8d5fac3c48ffc815727
esma_akdemir <1esmakdemir@gmail.com>
2023-06-08 22:05:00 +0300

    Initializer ve get/set metodları

---
+++ Issiz.py
@@ -0,0 +1,11 @@
+from Insan import Insan
+class Issiz(Insan):
+    def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, tecrube):
+        super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk)
+        self.__tecrube = tecrube
+
+    def get_tecrube(self):
+        return self.__tecrube
+
+    def set_tecrube(self, tecrube):
+        self.__tecrube = tecrube
